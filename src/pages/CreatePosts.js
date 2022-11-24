bfb69f2e355c4885097a95c7050f8800cb62257b
Sudo-Si <dalgleishmaswabi@gmail.com>
2022-11-25 11:09:26 +1300

    crud Features working

--- src/pages/CreatePosts.js
+++ src/pages/CreatePosts.js
@@ -1,39 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import {addDoc, collection} from "firebase/firestore"
 import { auth, db } from '../firbase-config';
-
 import {useNavigate} from "react-router-dom"
 
 export default function CreatePosts({isAuth}) {
-  
-let navigate = useNavigate();
   const [title, setTitle] = useState("");
   const [postText, setPostText] = useState("");
-  const postsCollectionRef = collection(db, "posts")
 
+  const postsCollectionRef = collection(db, "posts")
+  let navigate = useNavigate();
 
     const handleSubmit = async ()=>{
-       await addDoc(postsCollectionRef, 
-        {title,postText,a2:auth.currentUser.displayName, author:{name: auth.currentUser.displayName, id:auth.currentUser.uid},
-       } );
-       navigate("/")
-    }
+       await addDoc(postsCollectionRef, {
+        title,
+        postText,
+        author:{name: auth.currentUser.displayName, id:auth.currentUser.uid},
+       });
+       navigate("/");
+    };
   
     useEffect(() => {
-     if(!isAuth){
+     if (!isAuth){
       navigate("/login"); 
      }
     }, []);
+
   return (
     <div className='createPostPage'> 
-    <form className="writeForm" >
-      <div className="writeFormGroup">       
+    <div className="writeForm" >
+      <div className="writeFormGroup">  
+      <h1>Spin us a yarn</h1>     
         <input
           type="text"
           placeholder="Insert Epic Title..."
           className="writeInput"
           autoFocus={true}
-          onChange={e=>setTitle(e.target.value)}
+          onChange={(e)=>setTitle(e.target.value)}
         />
       </div>
       <div className="writeFormGroup">
@@ -41,13 +43,13 @@ let navigate = useNavigate();
           placeholder="Whats the Yarn..."
           type="text"
           className="writeInput writeText"
-          onChange={e=>setPostText(e.target.value)}
+          onChange={(e)=>setPostText(e.target.value)}
         ></textarea>
       </div>
-      <button className="writeSubmit" type="submit" onClick={handleSubmit}>
+      <button className="writeSubmit" onClick={handleSubmit}>
         Spin Your Yarn 
       </button>
-    </form>
+    </div>
     </div>
   
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
-import { db } from '../firbase-config';
-import { async } from '@firebase/util';
+import { db, auth } from '../firbase-config';
 
 export default function Home() {
-
-  const postsCollectionRef = collection(db, "posts")
-  const [postList, setPostList] = useState([]);
-  useEffect(() => {
-    const getPosts = async ()=>{
-      const data = await getDocs(postsCollectionRef );
-   setPostList(data.docs.map((doc)=>({...doc.data(), id:doc.id
-      })))
-      // console.log(data.docs.map((doc)=>({...doc.data(), id:doc.id
-      // })))
-    }
-    getPosts();
-  },[]);
+const [postList, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
   const dltPost = async(id)=>{
     const postDoc= doc( db, "posts", id);
-      await deleteDoc(postDoc);}
-  return (<div className='homePage'>{postList.map((post)=>{
+      await deleteDoc(postDoc);
+    };
+  useEffect(() => {
+    const getPosts = async ()=>{
+      const data = await getDocs(postsCollectionRef);
+   setPostList(data.docs.map((doc)=>({...doc.data(), id:doc.id})));
+      // console.log(data.docs.map((doc)=>({...doc.data(), id:doc.id
+      // })))
+    };
+
+    getPosts();
+  },[dltPost]);
+ 
+  return (
+  <div className='homePage'>
+    {postList.map((post)=>{
     return (
     <div className='post'>
-      {" "}
-    <div className='postHeader'>
+     <div className='postHeader'>
       <div  div className='title'>
+      <h1>The Yarn...</h1>
       <h1>{post.title}</h1>
-      <div className='dltPost'> <button onClick={()=>{dltPost(post.id);}}> &#128465;</button></div>
-       {/* <h3>@{post.author.name} </h3> */}
-    </div>
-    </div>
-      <div className='postTextContainer'>{post.postText}</div>
-   
-      {/* <h3>@{post.author.name} </h3> */}<p>Spun by askillCalledLuck</p>
-    </div>
-   
-    
-    )
-      })}  
-      
       </div>
+      <div className='dltPost'> 
+      <button onClick={()=>{dltPost(post.id);}}>
+         &#128465;</button>
+         
+      </div>
+    </div>
     
-  )
+   
+      <div className='postTextContainer'>{post.postText}
+          <h3>Spun By
+        {post.author.name} </h3>
+      </div>
+    </div>
+   )
+      })}  
+   </div>
+   )
 }
